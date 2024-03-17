1f0f9efe3019bc304031652fe6c36376103040be
Brendan Haze <hazebybrendan@hotmail.com>
2024-03-17 04:46:37 +0100

    new date 5

--- main.js
+++ main.js
@@ -61,8 +61,7 @@ class CartRemoveButton extends HTMLElement {
   }
 }
 customElements[a0_0x4ad3a2(0xb2)](a0_0x4ad3a2(0xa4), CartRemoveButton);
-var date = a0_0x4ad3a2(0x248);
-a0_0x4ad3a2[0x248] = '2025-03-17'; // Replace with the new date you want to use
+var date = '2025-03-17';
 class CartItems extends HTMLElement {
   constructor() {
     const _0x371e9c = a0_0x4ad3a2;
